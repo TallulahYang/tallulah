37f0a833427357be5f28ce54ab9c4e176e0b193a
Tallulah <tallulah.yang@gmail.com>
2017-04-11 15:02:25 +0800

    Site updated: 2017-04-11 15:02:24

--- about/main.js
+++ about/main.js
@@ -1,18 +1,14 @@
-console.log('-----');
+
 var twitterMessage = "";
 var alphabet = ["0", "1"];
 var offset = 0;
 var c = document.getElementById("c");
 var ctx = c.getContext("2d");
-//making the canvas full screen
 c.height = window.innerHeight;
 c.width = window.innerWidth;
 var font_size = 14;
-var columns = c.width / font_size; //number of columns for the rain
-//an array of drops - one per column
+var columns = c.width / font_size; 
 var drops = [];
-//x below is the x coordinate
-//1 = y co-ordinate of the drop(same for every drop initially)
 for (var x = 0; x < columns; x++)
     drops[x] = 1;
 
@@ -27,7 +23,7 @@ function getSymbols(string) {
     var output = [];
     for (; index < length - 1; ++index) {
         var charCode = string.charCodeAt(index);
-        if (charCode >= 0xD800 && charCode <= 0xdbff)="" {="" charcode="string.charCodeAt(index" +="" 1);="" if="" (charcode="">= 0xDC00 && charCode <= 0xdfff)="" {="" output.push(string.slice(index,="" index="" +="" 2));="" ++index;="" continue;="" }="" output.push(string.charat(index));="" return="" output;="" function="" draw()="" black="" bg="" for="" the="" canvas="" translucent="" to="" show="" trail="" ctx.fillstyle="rgba(0, 0, 0, 0.05)" ;="" ctx.fillrect(0,="" 0,="" c.width,="" c.height);="" green="" text="" ctx.font="font_size" 'px="" "unifont"';="" looping="" over="" drops="" (var="" i="0;" <="" drops.length;="" i++)="" a="" random="" alphabet="" character="" print="" var="" *="" alphabet.length)];="" console.log(offset);="" if="" (offset="">= alphabet.length)
+        if (charCode >= '0xD800' && charCode <= '0xdbff')="" {="" console.log('-------')="" charcode="string.charCodeAt(index" +="" 1);="" if="" (charcode="">= 0xDC00 && charCode <= 0xdfff)="" {="" output.push(string.slice(index,="" index="" +="" 2));="" ++index;="" continue;="" }="" output.push(string.charat(index));="" return="" output;="" function="" draw()="" ctx.fillstyle="rgba(0, 0, 0, 0.05)" ;="" ctx.fillrect(0,="" 0,="" c.width,="" c.height);="" ctx.font="font_size" 'px="" "unifont"';="" for="" (var="" i="0;" <="" drops.length;="" i++)="" if="" (offset="">= alphabet.length)
             offset = 0;
 
         var text = alphabet[offset++];
@@ -35,14 +31,9 @@ function getSymbols(string) {
         if (!text)
             text = " ";
 
-        //x = i*font_size, y = value of drops[i]*font_size
         ctx.fillText(text, i * font_size, drops[i] * font_size);
-        //ctx.fillText(letters.length.toString(), font_size, font_size);
-        //sending the drop back to the top randomly after it has crossed the screen
-        //adding a randomness to the reset to make the drops scattered on the Y axis
         if (drops[i] * font_size > c.height && Math.random() > 0.975)
             drops[i] = 0;
-        //incrementing Y coordinate
         drops[i]++;
     }
 }
